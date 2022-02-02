3efb78e60c0a89ca5b3167995a5f19b9e2fb10c4
Brian Vu <brianvu7@gmail.com>
2022-02-02 10:48:38 -0600

    commit tree b504ba4f97de910b86908698440528113047500c

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const writeCards = names => {
+    const array = [];
+
+    for(let name of names){
+        array.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+    }
+
+    return array;
+}
+
+const countDown = num => {
+    for(let i = num; i >= 0; i--){
+        console.log(i);
+    }
+}
